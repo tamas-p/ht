e82df75108cfa70489201409305dc05dcffc8d25
tamas-p <tamas.palagyi@gmail.com>
2016-05-05 18:28:53 +0200

    Changed how to get num cores/threads.

--- ht.c
+++ ht.c
@@ -1,25 +1,3 @@
-/* The MIT License (MIT)
-
-Copyright (c) 2016 tamas-p
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE. */
-
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,9 +8,17 @@ SOFTWARE. */
 //--------------------------------------------------------------------------------------------------
 
 // These command are used to retrive information from /proc/cpuinfo
+// Number of physical cores = count(physical id) x cpu cores
+// Number of logical cores = count(physycal id) x siblings
+//
 const char* cmd_cache = "cat /proc/cpuinfo  | grep 'cache size' | head -1 | awk '{ print $4 }'";
+
 const char* cmd_num_pcores = "cat /proc/cpuinfo | grep 'core id' | sort | uniq | wc -l";
 const char* cmd_num_lcores = "cat /proc/cpuinfo | grep 'core id' | sort | wc -l";
+
+const char* cmd_num_cpu = "cat /proc/cpuinfo | grep 'physical id' | sort | uniq | wc -l";
+const char* cmd_cores_per_cpu = "cat /proc/cpuinfo | grep 'cpu cores' | sort | uniq | awk '{ print $4 }'";
+const char* cmd_threads_per_cpu = "cat /proc/cpuinfo | grep 'siblings' | sort | uniq | awk '{ print $3 }'";
 
 // Helper function to execute commands
 int execute(const char* cmd) {
@@ -204,8 +190,11 @@ void run_two_tests(int num_pcores, int num_lcores, size_t cache_kb, void* test()
 
 int main(int argc, char *argv[]) {
   size_t cache_kb = execute(cmd_cache);
-  int num_pcores = execute(cmd_num_pcores);
-  int num_lcores = execute(cmd_num_lcores);
+  // int num_pcores = execute(cmd_num_pcores);
+  // int num_lcores = execute(cmd_num_lcores);
+
+  int num_pcores = execute(cmd_num_cpu) * execute(cmd_cores_per_cpu);
+  int num_lcores = execute(cmd_num_cpu) * execute(cmd_threads_per_cpu);
 
   printf("LLC Cache = %zu KB\n", cache_kb);
   printf("Number of physical cores = %d\n", num_pcores);
